85c3135c716a362c5ba0c666ae28f96ad05daea6
Nadezhda Klinskaya <npantelyeva@gmail.com>
2023-10-28 20:01:54 +0300

    feat: add solution for task Guession-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessing;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.guessing = Math.round((this.max - this.min) / 2);
     }
 
     guess() {
-
+        return this.guessing;
     }
 
     lower() {
-
+        this.max = this.guessing;
+        this.guessing = Math.round((this.max - this.min) / 2 + this.min);
     }
 
     greater() {
-
+        this.min = this.guessing;
+        this.guessing = Math.round((this.max - this.min) / 2 + this.min);
     }
 }
 
